06f708dfe3e86bc9742d1ddcda6234e818be4057
clarnx <clarenceonline@outlook.com>
2025-02-13 18:03:40 +0000

    added week 06 activity

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -1,9 +1,16 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Shapes Project.");
+        Square square = new Square("Red", 4);
+        Rectangle rectangle = new Rectangle("Blue", 5, 3);
+        Circle circle = new Circle("Green", 2.5);
+
+        List<Shape> shapes = new List<Shape> { square, rectangle, circle };
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape Color: {shape.GetColor()}, Area: {shape.GetArea():F2}");
+        }
     }
 }
